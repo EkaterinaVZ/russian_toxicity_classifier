c0955a451a574659b4fce8e6f0dfefe2f35e68fe
Екатерина Жериборова <artprojectt@mail.ru>
2022-04-08 22:52:19 +0500

    added file test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from russian_sensitive_topics import tokenizer, preds
 
+
 def test_post_predict_one():
     tokenized = tokenizer.batch_encode_plus(['взорвать дом'], max_length=40,
                                             pad_to_max_length=True,
